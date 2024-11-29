f9a396779aa63fe563d0a8a99f4db279120a2ce1
Sebastian Leitl <sebastian1125.leitl@gmail.com>
2024-11-30 00:00:58 +0100

    ADD: few methods for gameLogicRoulette

---
+++ casinoProject/roulette/rouletteGameLogic.py
@@ -0,0 +1,24 @@
+from typing import List
+from rouletteWheel import rouletteWheel
+from roulettePlayer import roulettePlayer
+from rouletteResult import Result
+from rouletteBet import rouletteBet
+
+class rouletteGameLogic:
+    wheel: rouletteWheel = None
+    players: List[roulettePlayer] = None
+    bets: List[rouletteBet] = []
+    
+
+    def __init__(self, playersAttributes: list):
+        self.wheel = rouletteWheel()
+        self.players = playersAttributes
+
+    def spinWheel(self) -> Result:
+        return self.wheel.spin()
+    
+    def placeBet(self, playerName: str, betType: str, betValue: str):
+        self.bets.append(rouletteBet(playerName, betType, betValue))
+
+    def calculatePayout(self, result: Result):
+        roulettePayoutCalculator = roulettePayoutCalculator()
