8e2319c6e48b05b4ae0262cd588dfa66927d7323
danie <upjeq@student.kit.edu>
2024-12-05 00:31:37 +0100

    fix: payoutCalculator für mehrere wetten von einem Spieler

--- casinoProject/roulette/roulettePayoutCalculator.py
+++ casinoProject/roulette/roulettePayoutCalculator.py
@@ -8,13 +8,16 @@ class roulettePayoutCalculator:
         self.result = result
 
     def calculatePayout(self):
-        payouts = {}
+        # Initialisiere das payouts Dictionary mit allen Spielern und einem Payout von 0
+        payouts = {bet.player_id: 0 for bet in self.bets}
+        # Berechne die Auszahlungen für jede Wette
         for bet in self.bets:
             if self.result in bet.winning_numbers:
-                winningFactor = len(bet.winning_numbers)
+                winningFactor = 36/len(bet.winning_numbers)
                 payout = bet.amount * winningFactor
             else:
                 payout = 0
-            payouts[bet.player_id] = payout
+            payouts[bet.player_id] += payout
+            
         return payouts
         
