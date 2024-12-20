336e4542e2f7d5c12fa7b7e98035f0c5174e5395
danie <upjeq@student.kit.edu>
2024-12-20 22:28:15 +0100

    databankAPi change for offline

--- casinoProject/databankApi/views.py
+++ casinoProject/databankApi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .supabase_client import fetch_data_from_table
 
 def data_view(request):
-    data = fetch_data_from_table("Games")
-    return render(request, 'testing.html', {'data': data})
+    #data = fetch_data_from_table("Games")
+    return render(request, 'testing.html', {'data': "data"})
 
 # Create your views here.
